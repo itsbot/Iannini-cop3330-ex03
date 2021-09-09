038853e4adbc468cda89d1b83c706c9be9b6487d
itsbot <patrickiannini@knights.ucf.edu>
2021-09-09 17:36:30 -0400

    Completed Exercise

--- src/main/java/Exercise03/Exercise03.java
+++ src/main/java/Exercise03/Exercise03.java
@@ -3,6 +3,17 @@
  *  Copyright 2021 Patrick Iannini
  */
 package Exercise03;
+import java.util.Scanner;
 
 public class Exercise03 {
+    public static void main(String[] args) {
+        System.out.println("Enter a quote");
+        Scanner sc = new Scanner(System.in);
+        String quote = sc.nextLine();
+        System.out.println("Who said it?");
+        Scanner sc1 = new Scanner(System.in);
+        String speaker = sc1.nextLine();
+
+        System.out.println(speaker+" says, "+"\""+quote+".\"");
+    }
 }
